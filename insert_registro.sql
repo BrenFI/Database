0adec26cfaa4d8729524293e3ecab89171685fcf
EC2 Default User <fi.brendag@hotmail.com>
2017-09-04 21:23:14 +0000

    Agregando archivos al repositorio en Github

---
+++ insert_registro.sql
@@ -0,0 +1,3 @@
+insert into registro(comanda_idcomanda,insum_idinsumo,fecha_inicio,fecha_fin,cantidad,unidad,importe, repeticiones) values(12,102,'10.02.17','20.05.17','1','cajas',700.99,1);
+insert into registro(comanda_idcomanda,insum_idinsumo,fecha_inicio,fecha_fin,cantidad,unidad,importe, repeticiones) values(11,103,'10.02.17','20.05.17','1','cajas',200.99,1);
+insert into registro(comanda_idcomanda,insum_idinsumo,fecha_inicio,fecha_fin,cantidad,unidad,importe, repeticiones) values(11,102,'10.02.17','20.05.17','1','cajas',100.50,1); 
