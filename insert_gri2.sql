bba87fd1c08ebcf4a39ee901677da7c0d82e98bc
EC2 Default User <fi.brendag@hotmail.com>
2017-09-06 20:47:54 +0000

    Subiendo categoria y subcategoria archivo

---
+++ insert_gri2.sql
@@ -0,0 +1,6 @@
+insert into gri(nombre)values('Laboratorio – Análisis clínicos');
+insert into gri(nombre)values('Laboratorio – Perfiles');
+insert into gri(nombre)values('Laboratorio – Estudios Especiales');
+insert into gri(nombre)values('Laboratorio – Patología');
+insert into gri(nombre)values('Laboratorio – Radiología');
+insert into gri(nombre)values('Laboratorio – Ultrasonidos');
